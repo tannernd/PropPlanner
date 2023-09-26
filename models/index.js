d5a9cedd85e82886130a053e696ba9025008d82d
Nate Tanner <40620787+tannernd@users.noreply.github.com>
2023-09-25 21:12:14 -0600

    Update and Delete Routes

--- models/index.js
+++ models/index.js
@@ -6,6 +6,103 @@ const Market = require('./Market');
 const Mortage = require('./Mortgage');
 const Property = require('./Property');
 
+Property.hasMany(Users, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
 
+Users.belongsTo(Property, {
+  foreignKey: 'user_id',
+});
+
+Mortage.hasMany(Users, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+  });
+  
+Users.belongsTo(Mortage, {
+foreignKey: 'user_id',
+});
+
+Market.hasMany(Users, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+  });
+  
+Users.belongsTo(Market, {
+foreignKey: 'user_id',
+});
+
+Income.hasMany(Users, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+  });
+  
+Users.belongsTo(Income, {
+foreignKey: 'user_id',
+});
+
+Financials.hasMany(Users, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+  });
+  
+Users.belongsTo(Financials, {
+foreignKey: 'user_id',
+});
+
+Expenses.hasMany(Users, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+  });
+  
+Users.belongsTo(Expenses, {
+foreignKey: 'user_id',
+});
+
+Property.hasMany(Mortage, {
+    foreignKey: 'prop_id',
+    onDelete: 'CASCADE',
+  });
+  
+Mortage.belongsTo(Property, {
+foreignKey: 'prop_id',
+});
+
+Property.hasMany(Market, {
+    foreignKey: 'prop_id',
+    onDelete: 'CASCADE',
+  });
+  
+Market.belongsTo(Property, {
+foreignKey: 'prop_id',
+});
+
+Property.hasMany(Income, {
+    foreignKey: 'prop_id',
+    onDelete: 'CASCADE',
+  });
+  
+Income.belongsTo(Property, {
+foreignKey: 'prop_id',
+});
+
+Property.hasMany(Financials, {
+    foreignKey: 'prop_id',
+    onDelete: 'CASCADE',
+  });
+  
+Financials.belongsTo(Financials, {
+foreignKey: 'prop_id',
+});
+
+Property.hasMany(Expenses, {
+    foreignKey: 'prop_id',
+    onDelete: 'CASCADE',
+  });
+  
+Expenses.belongsTo(Property, {
+foreignKey: 'prop_id',
+});
 
 module.exports = { Users, Expenses, Financials, Income, Market, Mortage, Property };
