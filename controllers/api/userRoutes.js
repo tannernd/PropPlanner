3dd25584fe8bd269b210b720714d90a6e1b6df3d
Nate Tanner <40620787+tannernd@users.noreply.github.com>
2023-09-28 23:05:24 -0600

    Scaffolding for dashboard, login, and other frontend

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { User } = require('../../models');
-const withAuth = require('../../utils/auth')
+const withAuth = require('../../utils/auth');
 
 //#region -- user create/update/delete
 router.post('/create', async (req, res) => {
--- public/js/example.js
+++ public/js/example.js
@@ -1,1 +1,4 @@
 console.log('Hello World!');
+
+$('#property_type').chosen({width: "100%"});
+$('#state').chosen({width: "100%"});
