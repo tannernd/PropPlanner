b85fa1fecce222b8aeac64ca49d8c22578aa69ec
Jared Boehm <me@jaredboehm.com>
2023-10-01 09:57:44 -0600

    Report Data

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,6 @@
 module.exports = {
   formatCurrency: (value) => {
-    value = "$" + value.replace(/(\d)(?=(\d{3})+(\.(\d){0,2})*$)/g, '$1,');
+    value = "$" + value.toString().replace(/(\d)(?=(\d{3})+(\.(\d){0,2})*$)/g, '$1,');
 
     if(value.indexOf('.') === -1)
         return value + '.00';
@@ -8,6 +8,8 @@ module.exports = {
     var decimals = value.split('.')[1];
 
     return decimals.length < 2 ? value + '0' : value;
-  }
+  },
+  ipp: (index) => parseInt(index) + 1,
+  limit: (value, length) => value.toString().slice(0, length),
+  suffix: (value, character) => (value.toString().endsWith(character)) ? value : value.toString() + character
 }
- 
